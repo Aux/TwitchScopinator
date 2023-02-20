9b150d5df2f72093a5d184dad750f1c2cba27a4f
Aux <justin@akitaux.com>
2023-02-19 23:55:02 -0600

    init

---
+++ src/TwitchScopinator/Models/ScopeInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TwitchScopinator.Models
+{
+    public class ScopeInfo
+    {
+        [JsonPropertyName("rest")]
+        public List<RestAction> Rest { get; set; } = new();
+
+        [JsonPropertyName("chat")]
+        public List<TwitchAction> Chat { get; set; } = new();
+
+        [JsonPropertyName("pubsub")]
+        public List<TwitchAction> PubSub { get; set; } = new();
+
+        [JsonPropertyName("eventsub")]
+        public List<EventSubAction> EventSub { get; set; } = new();
+    }
+}
---
+++ src/TwitchScopinator/Models/TwitchAction.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TwitchScopinator.Models
+{
+    public class TwitchAction
+    {
+        // Chat
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+
+        [JsonPropertyName("scopes")]
+        public string[]? Scopes { get; set; }
+
+        [JsonPropertyName("link")]
+        public string? Link { get; set; }
+    }
+
+    public class RestAction : TwitchAction
+    {
+        [JsonPropertyName("category")]
+        public string? Category { get; set; }
+        [JsonPropertyName("allow_app_auth")]
+        public bool? AllowsAppAuth { get; set; }
+    }
+
+    public class EventSubAction : RestAction
+    {
+        [JsonPropertyName("version")]
+        public string? Version { get; set; }
+
+        [JsonPropertyName("scope_required")]
+        public bool? IsScopeRequired { get; set; }
+    }
+}
